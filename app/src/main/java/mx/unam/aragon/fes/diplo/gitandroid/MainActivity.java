6238082c23a53ea82fa40a8e720f413b0f586288
ulsOP <ico.op.uls@gmail.com>
2018-02-10 13:40:22 -0600

    Programa para compartir con el profe

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -2,12 +2,29 @@ package mx.unam.aragon.fes.diplo.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button miBoton;
+    private TextView txtVista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        miBoton=findViewById(R.id.button3);
+        txtVista=findViewById(R.id.textView);
+
+        miBoton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                txtVista.setText("Hola GitHub");
+
+            }
+    });
+
     }
 }
